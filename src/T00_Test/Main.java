cbccf1c3802a759b24032487c6a9dd2eb9b2936a
icaynia <icaynia@icaynia.com>
2017-07-15 00:45:05 +0900

    First commit with test source

---
+++ src/T00_Test/Main.java
@@ -0,0 +1,14 @@
+package T00_Test;
+
+import java.io.IOException;
+
+/**
+ * Created by icaynia on 2017. 7. 15..
+ */
+
+public class Main {
+    public static void main(String[] args) throws IOException
+    {
+        System.out.println("Hello world");
+    }
+}
